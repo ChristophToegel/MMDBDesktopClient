cb65ef1b462d95f330a975254e6df001d1251cb5
Julian Dietz <julian.dietz@stud.uni-regensburg.de>
2016-07-24 21:03:50 +0200

    connection to db

--- src/main.java
+++ src/main.java
@@ -1,9 +1,52 @@
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import com.sun.xml.internal.fastinfoset.sax.Properties;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by Christoph on 06.07.16.
  */
 public class main {
 
+
+    private final static String USER="j.dietz";
+    private final static String PASSWORD ="mmdb";
     public static void main (String args[]){
-    System.out.print("TTTTTTTTTTTTTTTTTTtest");
+    System.out.print("test");
+
+        createDbConnection();
+        test();
+    }
+
+    private static void test() {
+         Connection conn =createDbConnection();
+
+        Statement test= null;
+        try {
+            test = (Statement) conn.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        String string="INSERT INTO employee (firstname,lastname) values ('hallo1','test1');";
+
+        try {
+            test.executeUpdate(string);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Connection createDbConnection() {
+        Connection conn = null;
+            try {
+                conn = (Connection) DriverManager.getConnection( "jdbc:mysql://132.199.139.24:3306/mmdb16_juliandietz",
+                        USER, PASSWORD);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        System.out.println("Connected to database");
+        return conn;
     }
 }
