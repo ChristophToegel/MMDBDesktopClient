dd497068749c9c37996f81c9426ef332736bb80e
Julian Dietz <julian.dietz@stud.uni-regensburg.de>
2016-08-03 20:22:25 +0200

    getter/setter driver

--- src/objects/driver.java
+++ src/objects/driver.java
@@ -9,6 +9,8 @@ import java.sql.SQLException;
  */
 public class driver {
 
+
+
     private int emp_id;
     private String emp_sign;
     private String firstname;
@@ -36,6 +38,81 @@ public class driver {
 
     public String getFirstname(){
         return firstname;
+    }
+
+    public int getEmp_id() {
+        return emp_id;
+    }
+    public void setEmp_id(int emp_id) {
+        this.emp_id = emp_id;
+    }
+
+    public String getEmp_sign() {
+        return emp_sign;
+    }
+
+    public void setEmp_sign(String emp_sign) {
+        this.emp_sign = emp_sign;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getDriver_id() {
+        return driver_id;
+    }
+
+    public void setDriver_id(int driver_id) {
+        this.driver_id = driver_id;
+    }
+
+    public int getLocation_id() {
+        return location_id;
+    }
+
+    public void setLocation_id(int location_id) {
+        this.location_id = location_id;
+    }
+
+    public int getVehicle_id() {
+        return vehicle_id;
+    }
+
+    public void setVehicle_id(int vehicle_id) {
+        this.vehicle_id = vehicle_id;
+    }
+
+    public int getSuper_manager() {
+        return super_manager;
+    }
+
+    public void setSuper_manager(int super_manager) {
+        this.super_manager = super_manager;
+    }
+
+    public int getEngaged() {
+        return engaged;
+    }
+
+    public void setEngaged(int engaged) {
+        this.engaged = engaged;
     }
 
 }
